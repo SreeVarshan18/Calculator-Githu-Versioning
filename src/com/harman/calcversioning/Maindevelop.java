4a7356af3e3a5f82bbd849d0da04f150f5cccc9a
SreeVarshan18 <varshan.sree18@gmail.com>
2022-03-16 12:52:09 +0530

    addition

--- src/com/harman/calcversioning/Maindevelop.java
+++ src/com/harman/calcversioning/Maindevelop.java
@@ -2,6 +2,10 @@ package com.harman.calcversioning;
 
 public class Maindevelop {
     public static void main(String[] args) {
+        int a=12,b=9;
+        addition addobj=new addition();
+        int result = addobj.Addnumbers(a,b);
+        System.out.println(result);
 
     }
 }
---
+++ src/com/harman/calcversioning/addition.java
@@ -0,0 +1,7 @@
+package com.harman.calcversioning;
+
+public class addition {
+    public int Addnumbers(int x, int y){
+        return x+y;
+    }
+}

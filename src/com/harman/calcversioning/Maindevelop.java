aa26243343d1977db07c2afc6c6d8c44be53b55a
SreeVarshan18 <varshan.sree18@gmail.com>
2022-03-16 13:02:37 +0530

    sub

--- src/com/harman/calcversioning/Maindevelop.java
+++ src/com/harman/calcversioning/Maindevelop.java
@@ -2,6 +2,10 @@ package com.harman.calcversioning;
 
 public class Maindevelop {
     public static void main(String[] args) {
+        int a=12,b=9;
+        Sub subobj= new Sub();
+        int subresult= subobj.Subnumbers(a,b);
+        System.out.println(subresult);
 
     }
 }
---
+++ src/com/harman/calcversioning/Sub.java
@@ -0,0 +1,8 @@
+package com.harman.calcversioning;
+
+public class Sub {
+
+    public int Subnumbers(int x, int y){
+        return x-y;
+    }
+}
